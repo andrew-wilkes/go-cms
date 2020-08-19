9346f28f4a9584e4f1c542efd188ee3301892e41
andrew-wilkes <github@urgentclick.com>
2020-08-19 23:01:18 +0900

    Add functions to main

--- src/main.go
+++ src/main.go
@@ -11,20 +11,47 @@ type response struct {
 	Content string
 }
 
+// This is the entry point function for the application
+// It is called (from index.php) with command line arguments representing
+// the GET and POST data that is received from the client by the web server
 func main() {
+	// Get the command line arguments
 	args := os.Args[1:]
+
+	// Extract the GET map
 	getArg := ""
 	if len(args) > 0 {
 		getArg = args[0]
 	}
+
+	// Extract the POST map
 	postArg := ""
 	if len(args) > 1 {
 		postArg = args[1]
 	}
+
+	headers, content := processInput(getArg, postArg)
+
+	// Construct the response containing headers and content
 	resA := &response{
-		Headers: []string{"apple", "peach", "pear"},
-		Content: fmt.Sprintf("The content. Get: %s Post: %s", getArg, postArg),
+		Headers: headers,
+		Content: content,
 	}
+	// Encode the response in text format
 	resB, _ := json.Marshal(resA)
+
+	// Output the response
 	fmt.Print(string(resB))
+}
+
+func processInput(getArg string, postArg string) ([]string, string) {
+	return headers(), content(getArg, postArg)
+}
+
+func headers() []string {
+	return []string{"apple", "peach", "pear"}
+}
+
+func content(getArg string, postArg string) string {
+	return fmt.Sprintf("The content. Get: %s Post: %s", getArg, postArg)
 }
