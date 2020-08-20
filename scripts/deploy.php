bb92e7d303c6affa27c2f7f4c29d42ee80646cd3
andrew-wilkes <github@urgentclick.com>
2020-08-21 04:46:48 +0900

    Inject app path into index.php

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -1,12 +1,11 @@
 <?php
 
 $web_root = "/usr/share/nginx/www/gocms.com";
+$server_script_path = "/usr/share/nginx/apps/gocms";
 
 $dir = dirname(__DIR__);
 
 $index = $dir . "/www/index.php";
-
-include_once("$index");
 
 if (!file_exists($server_script_path)) die("$server_script_path path invalid!");
 
@@ -15,11 +14,7 @@ $gocms = $dir . "/build/gocms";
 if (!file_exists($gocms)) die("gocms build file is missing!\n");
 
 // Copy gocms program to server
-$cmd = "cp $gocms $server_script_path";
-echo("$cmd\n");
-exec($cmd);
+exec("cp $gocms $server_script_path");
 
-// Copy index.php to server
-$cmd = "cp $index $web_root";
-echo("$cmd\n");
-exec($cmd);
+// Inject server_script_path into index.php and copy to server
+file_put_contents($web_root . "/index.php", str_replace('server_script_path', $server_script_path, file_get_contents($index)));
--- www/index.php
+++ www/index.php
@@ -3,20 +3,16 @@
     The gocms PHP index file
 */
 
-$server_script_path = "/usr/share/nginx/apps/gocms";
-
-if (basename(__DIR__) != "www") {
-    $cmd = sprintf("%s/gocms %s %s", $server_script_path, json_encode((object)$_GET), json_encode((object)$_POST));
-    exec($cmd, $output, $code);
-    if ($code == 0) {
-        $response = json_decode($output[0]);
-        foreach ($response->Headers as $header)
-            if ($header == 404)
-                header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-            else
-                header($header);
-        echo $response->Content;
-    }
-    else
-        echo "Error code: " . $code;
+$cmd = sprintf("server_script_path/gocms %s %s", json_encode((object)$_GET), json_encode((object)$_POST));
+exec($cmd, $output, $code);
+if ($code == 0) {
+    $response = json_decode($output[0]);
+    foreach ($response->Headers as $header)
+        if ($header == 404)
+            header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        else
+            header($header);
+    echo $response->Content;
 }
+else
+    echo "Error code: " . $code;
