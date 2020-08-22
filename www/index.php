bc0d2dcd55f082f4e679eeb834760dce55518786
andrew-wilkes <github@urgentclick.com>
2020-08-22 15:29:22 +0900

    Tidy up index.php code

--- www/index.php
+++ www/index.php
@@ -1,70 +1,65 @@
 <?php
-/*
-    The gocms PHP index file
-*/
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $_SERVER['DATA'] = file_get_contents("php://input");
-  $_SERVER['POST'] = (object)$_POST;
-}
-$_SERVER['GET'] = (object)$_GET;
+$_SERVER['RAW_DATA'] = file_get_contents("php://input");
+$_SERVER['POST_DATA'] = (object)$_POST;
+$_SERVER['GET_DATA'] = (object)$_GET;
 
 file_put_contents(sprintf("log/args%s.json", $_SERVER['REQUEST_TIME_FLOAT']), json_encode($_SERVER));
 
 $cmd = sprintf("server_script_path/gocms %s", json_encode($_SERVER));
 exec($cmd, $output, $code);
 if ($code == 0) {
-    $response = json_decode($output[0]);
-    foreach ($response->Headers as $header)
-        header($header);
-    echo $response->Content;
+  $response = json_decode($output[0]);
+  foreach ($response->Headers as $header)
+    header($header);
+  echo $response->Content;
 }
 else
-    echo "Error code: " . $code;
+  echo "Error code: " . $code;
 ?>
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<head>
-  <title>Go CMS Test Page</title>
-  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-</head>
-<body>
-  
-  <script>
-    // Make a request for a user with a given ID
-  axios.get('/user?ID=12345')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+  <head>
+    <title>Go CMS Test Page</title>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  </head>
+  <body>
+    <script>
+      // Make a request for a user with a given ID
+      axios.get('/user?ID=12345')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
 
-    // Performing a POST request
-    axios.post('/user', {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      // Perform a POST request
+      axios.post('/user', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
-    let data = new FormData();
-    data.append('binary', Uint8Array.from('test', c => c.charCodeAt(0)));
-    axios.post('/form-user', data)
-    .then(function (response) {
-    console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  </script>
-</body>
+      // Perform a Form data submission
+      let data = new FormData();
+      data.append('binary', Uint8Array.from('test', c => c.charCodeAt(0)));
+      axios.post('/form-user', data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    </script>
+  </body>
 </html>
