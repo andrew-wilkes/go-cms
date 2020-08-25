1b3adacf05b3404b78d69989b28032d7b084e10a
andrew-wilkes <github@urgentclick.com>
2020-08-25 14:33:53 +0900

    Have log deploy mode

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -1,4 +1,8 @@
 <?php
+
+// Add command line param of -l or --log to output the test version of index.php
+$test_mode = 0 < count(getopt("l", ["log"]));
+
 define("USING_TOOL", true);
 
 include_once(__DIR__ . "/config.php");
@@ -20,5 +24,17 @@ if (!file_exists($gocms)) die("The gocms app file is missing!" . PHP_EOL);
 // Copy gocms program to server
 exec("cp $gocms $server_script_path");
 
+// Formulate the web page index code
+$log_code = '$log_file_name = urlencode($_SERVER[\'REQUEST_URI\']);
+file_put_contents(sprintf(\'log/args-%s.json\', $log_file_name), json_encode($_SERVER));';
+$php_code = file_get_contents($index);
+if ($test_mode) {
+    $php_code = str_replace("#LOG\n", $log_code, $php_code);
+    $php_code = str_replace("#HTML", file_get_contents($dir . "/www/test.html"), $php_code);
+} else {
+    $php_code = str_replace("\n#LOG", "", $php_code);
+    $php_code = str_replace("\n#HTML", "", $php_code);
+}
+
 // Inject server_script_path into index.php and copy to server
-file_put_contents($web_root . "/index.php", str_replace('server_script_path', $server_script_path, file_get_contents($index)));
+file_put_contents($web_root . "/index.php", str_replace('server_script_path', $server_script_path, $php_code));
--- www/index.php
+++ www/index.php
@@ -2,10 +2,7 @@
 $_SERVER['RAW_DATA'] = file_get_contents("php://input");
 $_SERVER['POST_DATA'] = (object)$_POST;
 $_SERVER['GET_DATA'] = (object)$_GET;
-
-$log_file_name = urlencode($_SERVER['REQUEST_URI']);
-file_put_contents(sprintf("log/args-%s.json", $log_file_name), json_encode($_SERVER));
-
+#LOG
 $cmd = sprintf("server_script_path/gocms %s", json_encode($_SERVER));
 exec($cmd, $output, $code);
 if ($code == 0) {
@@ -13,58 +10,5 @@ if ($code == 0) {
   foreach ($response->Headers as $header)
     header($header);
   echo $response->Content;
-}
-else
-  echo "Error code: " . $code;
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Go CMS Test Page</title>
-    <meta charset="utf-8">
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-  </head>
-  <body>
-    <script>
-      // Generate the various kinds of http requests that we may expect to handle
-      // Using browser development tools and F12 we may observe what happens in detail
-      // Do a GET request for a user with a given ID
-      axios.get('/get-user?ID=12345')
-        .then(function (response) {
-          // handle success
-          console.log(response);
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
-
-      // Do a POST request with application/json content
-      axios.post('/single-page-app-user', {
-        firstName: 'Fred',
-        lastName: 'Flintstone'
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-      // Do a POST of regular form data
-      $.post("/form-submit-user", { name: "John", time: "2pm" });
-
-      // Do a multi-part form data submission such as used for file uploads
-      let data = new FormData();
-      data.append('binary', Uint8Array.from('test', c => c.charCodeAt(0)));
-      axios.post('/file-upload-form-user', data)
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    </script>
-  </body>
-</html>
+} else echo "Error code: " . $code;
+#HTML
