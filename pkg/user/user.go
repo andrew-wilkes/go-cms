4ee12835ad4a97ca5527cc9caf2034b54d6581ab
andrew-wilkes <github@urgentclick.com>
2020-08-28 03:35:45 +0900

    Add user package

---
+++ pkg/user/user.go
@@ -0,0 +1,21 @@
+package user
+
+// Status - returns the login status of the user
+func Status() {
+
+}
+
+// LogOn to the system
+func LogOn(email string, password string) {
+
+}
+
+// LogOff from the system
+func LogOff() {
+
+}
+
+// Register with the system
+func Register(name string, email string, password string) {
+
+}
